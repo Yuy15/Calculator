30a613aad89a438f37a9540b1fb5a1178187d0c7
nagi-navi <naginagi.hoby@gmail.com>
2022-03-02 12:57:30 +0900

    vue

--- static/js/Vue.js
+++ static/js/Vue.js
@@ -1,6 +1,18 @@
-var main = new Vue({
-    el: "#main",
+const tgt_input = new Vue({
+    el: "#tgt_input",
     data: {
         tgt: ""
     }
-})
+});
+
+const main = new Vue({
+    el: "#tgt_vue",
+    data: {
+        tgt_vue_d: ""
+    },
+    computed:{
+        tgt_vue:function(){
+            return tgt_input.tgt
+        }
+    }
+});
